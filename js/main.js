8b9e4c57f31f5d0429a71bd2f75d4b3b3a35685e
Hannah Erickson <hannaherickson@Hannahs-MacBook-Air.local>
2019-02-12 17:02:26 -0800

    intial commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+var countValue = null;
+var userInput = null;
+var countDisplay;
+var minusBtn;
+var plusBtn;
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    
+    countDisplay = document.getElementById("countValue");
+    userInput = document.getElementById('userInput').value;
+    
+    for (var i = 0; i < 1; i++) {
+        minusBtn[i].addEventListener('click', function(e){
+            e.target.countDisplay
+            i--;
+        })
+
+    for (var i = 0; i < 1; i++){
+        plusBtn[i].addEventListener('click', function(e){
+            e.target.countDisplay
+            i++;
+            });
+        }
+    } 
+});
+
